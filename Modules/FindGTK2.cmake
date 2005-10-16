2828626d161357783add262f8a58b3d09a85b55b
malat <malat>
2005-10-16 00:49:09 +0000

    ENH: Assume that everything is working, wxWidgets is a pain to use...

--- Modules/FindGTK2.cmake
+++ Modules/FindGTK2.cmake
@@ -11,13 +11,20 @@ IF(WIN32)
 
 ELSE(WIN32)
 
+  INCLUDE( "${CMAKE_CURRENT_SOURCE_DIR}/Modules/UseConfig.cmake")
   #Find new pkg-config
   FIND_PROGRAM(CMAKE_PKG_CONFIG pkg-config ../gtk2/bin ../../gtk2/bin)
 
   #if PKG is found
   IF(CMAKE_PKG_CONFIG)
-    SET(CMAKE_GTK2_CXX_FLAGS "`${CMAKE_PKG_CONFIG} --cflags gtk+-2.0`")
-    SET(GTK2_LIBRARIES "`${CMAKE_PKG_CONFIG} --libs gtk+-2.0`")
+    #SET(CMAKE_GTK2_CXX_FLAGS "`${CMAKE_PKG_CONFIG} --cflags gtk+-2.0`")
+    #SET(GTK2_LIBRARIES "`${CMAKE_PKG_CONFIG} --libs gtk+-2.0`")
+    FIND_LIBPATH( ${CMAKE_PKG_CONFIG}  "--libs gtk+-2.0")
+    FIND_INCLUDEPATH(${CMAKE_PKG_CONFIG} "--cflags gtk+-2.0")
+  SET(GTK_INCLUDE_PATH ${CONFIG_INCLUDE_PATH})
+  SET(GTK_LIBRARIES ${CONFIG_LIBRARIES})
+  #MESSAGE("${CONFIG_INCLUDE_PATH}")
+
   ENDIF(CMAKE_PKG_CONFIG)
   
 ENDIF(WIN32)  
