27db4d40ab086e3cf5f4bb27bddf02b15c8208c4
malat <malat>
2005-10-16 00:19:31 +0000

    ENH: FindGTK1 almost working still trouble with _gl and wx 2.4.x (no --gl-libs option)

--- Modules/FindGTK1.cmake
+++ Modules/FindGTK1.cmake
@@ -12,35 +12,42 @@ IF(WIN32)
 ELSE(WIN32)
 
   #Find new pkg-config
-  FIND_PROGRAM(CMAKE_PKG_CONFIG pkg-config 
-    ../gtk2/bin 
-    ../../gtk2/bin
-    )
+  #  FIND_PROGRAM(CMAKE_PKG_CONFIG pkg-config 
+  #    ../gtk2/bin 
+  #    ../../gtk2/bin
+  #    )
 
   #if PKG is found
   IF(CMAKE_PKG_CONFIG)
 
-    SET(CMAKE_GTK1_CXX_FLAGS "`${CMAKE_PKG_CONFIG} --cflags gtk`")
-    SET(GTK1_LIBRARIES "`${CMAKE_PKG_CONFIG} --libs gtk`")
+    #SET(CMAKE_GTK1_CXX_FLAGS "`${CMAKE_PKG_CONFIG} --cflags gtk`")
+    #SET(GTK1_LIBRARIES "`${CMAKE_PKG_CONFIG} --libs gtk`")
+    FIND_LIBPATH( ${CMAKE_PKG_CONFIG}  "--libs gtk")
+    FIND_INCLUDEPATH(${CMAKE_WX_CONFIG} "--cflags gtk")
+    #MESSAGE("${CONFIG_INCLUDE_PATH}")
+    #MESSAGE("${CONFIG_LIBRARIES}")
+    SET(GTK1_INCLUDE_PATH ${CONFIG_INCLUDE_PATH})
+    SET(GTK1_LIBRARIES ${CONFIG_LIBRARIES})
 
   ELSE(CMAKE_PKG_CONFIG)
     #we were not able to find to, lets try with:
     #old gtk-config
     FIND_PROGRAM(CMAKE_GTK_CONFIG gtk-config ../gtk/bin ../../gtk/bin)
 
-    SET(CMAKE_GTK1_CXX_FLAGS "`${CMAKE_GTK_CONFIG} --cflags`")
-    SET(GTK1_LIBRARIES "`${CMAKE_GTK_CONFIG} --libs`")
+    #SET(CMAKE_GTK1_CXX_FLAGS "`${CMAKE_GTK_CONFIG} --cflags`")
+    #SET(GTK1_LIBRARIES "`${CMAKE_GTK_CONFIG} --libs`")
+    FIND_LIBPATH( ${CMAKE_GTK_CONFIG}  "--libs")
+    FIND_INCLUDEPATH(${CMAKE_GTK_CONFIG} "--cflags")
+    SET(GTK1_INCLUDE_PATH ${CONFIG_INCLUDE_PATH})
+    SET(GTK1_LIBRARIES ${CONFIG_LIBRARIES})
 
-    MESSAGE(FATAL_ERROR "GTK2.x was not found but GTK1.x was, please set GTK_VERSION_1
-    to ON")
   ENDIF(CMAKE_PKG_CONFIG)
   
 ENDIF(WIN32)  
 
 MARK_AS_ADVANCED(
-  CMAKE_GTK_CXX_FLAGS
-  CMAKE_GTK2_CXX_FLAGS
-  GTK_INCLUDE_DIR
+  CMAKE_PKG_CONFIG
+  CMAKE_GTK_CONFIG
 )
 
 
